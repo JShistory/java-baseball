384aae091adb8f6664f4daa1485d1bb43897c3ca
KimJaeSung <behappy0916@naver.com>
2022-11-23 15:59:54 +0900

    초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,12 +5,6 @@ import baseball.controller.Controller;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Controller game = new Controller();
-        boolean play = true;
-        while(play){
-            game.playGame();
-            play = !game.getGameEnd();
-        }
 
     }
 }
