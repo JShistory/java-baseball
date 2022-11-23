04c40bb9d6260eed60ca2b9b3d3075936f3b711e
KimJaeSung <behappy0916@naver.com>
2022-11-23 17:59:17 +0900

    feat: 스트라이크 개수와 볼 개수를 판단하는 기능 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,37 @@
+package baseball;
+
+import baseball.User;
+import java.util.List;
+
+public class BaseballGame {
+    private User user;
+
+    private int strike;
+    private int ball;
+
+    public BaseballGame(){
+        user = new User();
+    }
+
+    public void countBall(List<Integer> computer, List<Integer> user){
+        for(int i=0; i<computer.size(); i++){
+            if(computer.contains(user.get(i))){
+                countStrike(computer.indexOf(computer.get(i)),i);
+            }
+        }
+    }
+
+    public void countStrike(int computerIndex, int index){
+        if(computerIndex == index){
+            strike ++;
+            ball--;
+        }
+        ball++;
+    }
+    public int getStrike(){
+        return strike;
+    }
+    public int getBall(){
+        return ball;
+    }
+}
