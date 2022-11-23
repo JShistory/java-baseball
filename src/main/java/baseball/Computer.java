99fcf925b33e084410a448c1c2575aa07b6bdb05
KimJaeSung <behappy0916@naver.com>
2022-11-23 17:32:21 +0900

    feat: 컴퓨터가 랜덤 값을 생성하는 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> computer;
+
+    public Computer(){
+        this.computer = new ArrayList<>();
+    }
+    public void createComputerNumbers(){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+    public List<Integer> getComputerNumbers(){
+        return computer;
+    }
+}
