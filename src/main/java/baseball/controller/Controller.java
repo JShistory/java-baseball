241214a974d97708e05540b96fc21ec2f2b8bc59
KimJaeSung <behappy0916@naver.com>
2022-11-28 11:20:42 +0900

    feat: 게임을 작동시키는 controller구현

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,6 +1,58 @@
 package baseball.controller;
 
-public class Controller {
+import baseball.BaseballGame;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import java.util.List;
 
+public class Controller {
+    private final String OVERLAP_NUMBERS = "[ERROR] 숫자가 중복되었습니다.";
+
+    private InputView inputView;
+    private OutputView outputView;
+    private BaseballGame baseballGame;
+
+    public Controller() {
+        inputView = new InputView();
+        outputView = new OutputView();
+    }
+
+    public void start() {
+        boolean gamePlay = true;
+        while (gamePlay == true) {
+            baseballGame = new BaseballGame();
+            inputUserNumbers();
+            outputView.printSuccess();
+            gamePlay = retry();
+        }
+    }
+
+    public void inputUserNumbers() {
+        boolean gameEnd = false;
+        while (!gameEnd) {
+            String userNumbers = inputView.inputNumber();
+            validateUserNumbers(userNumbers);
+            baseballGame.countStrikeBall(userNumbers);
+            gameEnd = baseballGame.successGame();
+            outputView.printResult(baseballGame.getStrike(), baseballGame.getBall());
+        }
+    }
+
+    public boolean retry() {
+        int inputRetry = inputView.retryNumber();
+        if(inputRetry == 2){
+            return false;
+        }
+        return true;
+    }
+
+
+
+    private void validateUserNumbers(String userNumbers) {
+        List<String> user = List.of(userNumbers.split(""));
+        if(user.size() != user.stream().distinct().count()){
+            throw new IllegalArgumentException(OVERLAP_NUMBERS);
+        }
+    }
 
 }
